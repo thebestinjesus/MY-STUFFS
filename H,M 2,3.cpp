d969c60c34cfe8e3af5b123ba6a3311749be33a5
thebestinjesus <90573182+thebestinjesus@users.noreply.github.com>
2025-02-19 23:41:08 +0200

    Add files via upload

---
+++ H,M 2,3.cpp
@@ -0,0 +1,27 @@
+// H,M 2,3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int A[10], B[10], i, j;
+	for (i = 0; i <= 9; i++) {
+		cout << "please enter the number with the index " << i << endl;
+		cin >> A[i];
+	}
+	for (i = 0; i <= 9; i++) {
+		B[i] = A[9 - i];
+	}
+	cout << "The original aray " << endl;
+	for (i = 0; i <= 9; i++) {
+		cout << A[i] << "-";
+	}
+	cout<< endl;
+	cout << "The new aray " << endl;
+	for (i = 0; i <= 9; i++) {
+		cout << B[i] << "-";
+	}
+
+}
+
