ff06d82eefb800e14cc38822e07d97e65fccaef6
thebestinjesus <90573182+thebestinjesus@users.noreply.github.com>
2025-03-04 07:50:25 +0200

    Add files via upload

---
+++ HM 3.2.1.cpp
@@ -0,0 +1,29 @@
+// HM 3.2.1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+	int index;
+	int i;
+	char string[20];
+	char string1[20];
+	cout << "Enter a string: ";
+	cin >> string;
+	cin.ignore();
+	cout << "The length of string1 is " << strlen(string) << endl;
+
+	strcpy_s(string1, 20, string);
+
+	i= strlen(string1);
+	cout << i;
+	cout << endl;
+
+	for (int index = i - 1; index >= 0; index--)
+	{
+		cout << string1[index];
+	}
+	return 0;
+}
